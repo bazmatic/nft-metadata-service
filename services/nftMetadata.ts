18b393db3151aadbfc05f24a6e477a0bf38f4e03
Barry Earsman <barry@earsman.com>
2022-08-08 10:24:56 +1000

    Return timestamp in response

--- services/nftMetadata.ts
+++ services/nftMetadata.ts
@@ -6,26 +6,27 @@ export class NftMetaDataService {
     constructor(private signer: Signer) {}
 
     async getMetadataUrl(contractAddress: string, tokenId: string): Promise<[string]> {
-        const contract = await getErc721Contract(contractAddress, this.signer);
-        const metadata = await contract.functions.tokenURI(tokenId);
-        return metadata;
+        const contract = await getErc721Contract(contractAddress, this.signer)
+        const metadata = await contract.functions.tokenURI(tokenId)
+        return metadata
     }
 
     async getOwner(contractAddress: string, tokenId: string): Promise<string> {
-        const contract = await getErc721Contract(contractAddress, this.signer);
-        const owner = await contract.functions.ownerOf(tokenId);
-        return owner[0];
+        const contract = await getErc721Contract(contractAddress, this.signer)
+        const owner = await contract.functions.ownerOf(tokenId)
+        return owner[0]
     }
 
     async getMetadata(contractAddress: string, tokenId: string): Promise<[string]> {
-        let [metadataUrl] = await this.getMetadataUrl(contractAddress, tokenId);
+        let [metadataUrl] = await this.getMetadataUrl(contractAddress, tokenId)
         // Extract the protocol
         const [protocol, ref] = metadataUrl.split("://");
         if (protocol === "ipfs") {
-            metadataUrl = `https://ipfs.io/ipfs/${ref}`;   
+            metadataUrl = `https://ipfs.io/ipfs/${ref}`
         }
         const metadata = await axios.get(metadataUrl);
-        metadata.data.owner = (await this.getOwner(contractAddress, tokenId));
-        return metadata.data;
+        metadata.data.owner = (await this.getOwner(contractAddress, tokenId))
+        metadata.data.timestamp = Date.now()
+        return metadata.data
     }
 }
