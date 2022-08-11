d5f95207aa9d6d90bd0cd78cb67930bee54e5dc1
Barry Earsman <barry@earsman.com>
2022-08-11 16:48:52 +1000

    Remove experiment

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,14 +21,12 @@ const Home: NextPage<Props> = ({host}) => {
         <h1 className={styles.title}>
           NFT Metadata Service
         </h1>
-
         <p className={styles.description}>
           This is a web service for fetching NFT metadata and images.
         </p>
         <p>
           To fetch the metadata for an NFT, make a request to:
         </p>
-        <Link href="/api/nft/nft/mainnet/0xD6F75a20Aa64634743D40fAfa88473020302C530/metadata/5">Hello</Link>
         <code className={styles.code}>http://{host}/api/nft/[network]/[contract address]/metadata/[token ID]</code>
         <p>
           To fetch the image for an NFT, make a request to:
